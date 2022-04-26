c09a4c739acbcc7feea8f52a4b7889e5dfd5b4c8
QBush <ignor-KuBush@yandex.ru>
2022-04-26 23:16:12 +1000

    DZ4_Events Промежуточное

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,10 +2,21 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import ru.netology.nmedia.databinding.PostListItemBinding
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(R.layout.post_list_item) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+//        val binding = PostListItemBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        binding.likes.setOnClickListener {
+//            binding.likes.setImageResource(R.drawable.ic_likes_24)
+//        }
     }
+
 }
