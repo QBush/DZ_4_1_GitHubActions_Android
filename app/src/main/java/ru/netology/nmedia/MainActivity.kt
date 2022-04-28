60c3bb3f10c0d8b55d9e6ab0a0b20ba516cce355
QBush <ignor-KuBush@yandex.ru>
2022-04-28 20:05:22 +1000

    DZ4_Events Промежуточное

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -3,20 +3,33 @@ package ru.netology.nmedia
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import ru.netology.nmedia.databinding.ActivityMainBinding
 import ru.netology.nmedia.databinding.PostListItemBinding
 
 
-class MainActivity : AppCompatActivity(R.layout.post_list_item) {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        setContentView(R.layout.post_list_item)
 
-//        val binding = PostListItemBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
-//
-//        binding.likes.setOnClickListener {
-//            binding.likes.setImageResource(R.drawable.ic_likes_24)
-//        }
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val post = Post (
+        id = 1,
+        author = "Netology",
+        content = "Привет! Это Нав... Это Нетология!",
+        published = "28.04.2022"
+        )
+
+
+        binding.post.likes.setOnClickListener {
+            post.likedByMe = !post.likedByMe
+            val imageResId = if(post.likedByMe) R.drawable.ic_liked_24 else R.drawable.ic_like_24
+            binding.post.likes.setImageResource(imageResId)
+        }
     }
+
 
 }
