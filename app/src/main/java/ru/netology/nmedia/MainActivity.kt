8ed9d24d2b1794c1176b235643c771c8bc032ad0
QBush <ignor-KuBush@yandex.ru>
2022-04-28 23:29:28 +1000

    DZ4_Events Промежуточное

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -9,11 +9,13 @@ import ru.netology.nmedia.databinding.PostListItemBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private var likeCount = 0
+    private var shareCount = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.post_list_item)
 
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val binding = PostListItemBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val post = Post(
@@ -24,29 +26,52 @@ class MainActivity : AppCompatActivity() {
         )
 
         with(binding) {
-            postListItem.authorName.text = post.author
-            postListItem.date.text = post.published
-            postListItem.postText.text = post.content
+            authorName.text = post.author
+            date.text = post.published
+            postText.text = post.content
             if (post.likedByMe) {
-                postListItem.likes.setImageResource(R.drawable.ic_liked_24)
+                likes.setImageResource(R.drawable.ic_liked_24)
             }
         }
 
-        binding.postListItem.likes.setOnClickListener {
+        binding.likes.setOnClickListener {
             post.likedByMe = !post.likedByMe
             val imageResId =
-                if (post.likedByMe) R.drawable.ic_liked_24 else R.drawable.ic_like_24
-            binding.postListItem.likes.setImageResource(imageResId)
+                if (post.likedByMe) {
+                    binding.likesCount.text = (likeCount++).toString()
+                    R.drawable.ic_liked_24
+                } else {
+                    binding.likesCount.text = (likeCount--).toString()
+                    R.drawable.ic_like_24
+                }
+            binding.likes.setImageResource(imageResId)
         }
+
+        binding.share.setOnClickListener {
+            binding.sharedCount.text = (shareCount++).toString()
+        }
+
     }
-}
-//    private fun ActivityMainBinding.render(post: Post) {
-//        postListItem.authorName.text = post.author
-//        postListItem.date.text = post.published
-//        postListItem.postText.text = post.content
+
+    private fun thousentKChanger(number: Long): String =
+        when (number) {
+            in 1..1000 -> number.toString()
+            in 1001..9999 -> "${String.format("%.1f", (number.toDouble() / 1000))}K"
+            in 10_000..999_999 -> "${number/1000}K"
+            else -> "${number/1_000_000}M"
+        }
+
+//private fun PostListItemBinding.render(post: Post) {
+//    authorName.text = post.author
+//    date.text = post.published
+//    postText.text = post.content
+//    if (post.likedByMe) {
+//        likes.setImageResource(R.drawable.ic_liked_24)
 //    }
+//}
 //
 //    private fun getLikeIconResId (liked: Boolean) =
 //        if (liked) R.drawable.ic_liked_24 else R.drawable.ic_like_24
-//
-//}
+
+
+}
