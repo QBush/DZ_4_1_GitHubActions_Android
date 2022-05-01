8efbc7e5e6e123f70c5b4d3ea818fdc71f87b575
QBush <ignor-KuBush@yandex.ru>
2022-05-01 19:51:28 +1000

    DZ5_Events Первые изменения

---
+++ app/src/main/java/ru/netology/nmedia/impl/InMemoryPostRepository.kt
@@ -0,0 +1,28 @@
+package ru.netology.nmedia.impl
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import ru.netology.nmedia.Post
+import ru.netology.nmedia.data.PostRepository
+
+class InMemoryPostRepository : PostRepository {
+
+    override val data = MutableLiveData<Post>(
+        Post(
+            id = 1,
+            author = "Netology",
+            content = "Привет! Это Нав... Это Нетология!",
+            published = "28.04.2022"
+        )
+    )
+
+
+    override fun like() {
+        val currentPost = checkNotNull(data.value) {
+            "value should not be null"
+        }
+        val likedPost = currentPost.copy(
+            likedByMe = !currentPost.likedByMe
+        )
+    }
+}
