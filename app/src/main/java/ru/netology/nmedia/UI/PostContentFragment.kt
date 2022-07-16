6a1071fa49f295e20128c1f57fb8f4b5b793b6ee
QBush <ignor-KuBush@yandex.ru>
2022-07-16 23:16:29 +1000

    DZ12 SQL Промежуточное

--- app/src/main/java/ru/netology/nmedia/UI/PostContentFragment.kt
+++ app/src/main/java/ru/netology/nmedia/UI/PostContentFragment.kt
@@ -1,15 +1,22 @@
 package ru.netology.nmedia.UI
 // Активити для редактирования и создания поста
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.activity.addCallback
+import androidx.core.content.edit
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResult
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import ru.netology.nmedia.PostEditableContent
+import ru.netology.nmedia.data.impl.SharedPrefsPostRepository
 import ru.netology.nmedia.databinding.PostContentFragmentBinding
 import ru.netology.nmedia.viewModel.PostViewModel
 
@@ -18,10 +25,9 @@ class PostContentFragment : Fragment() {
     private val viewModel by viewModels<PostViewModel>(ownerProducer = ::requireParentFragment)
     private val args by navArgs<PostContentFragmentArgs>()
 
-//    private val initialText
-//        get() = requireArguments().getString(INITIAL_TEXT_KEY)
-//    private val initialUrl
-//        get() = requireArguments().getString(INITIAL_URL_KEY)
+    private val prefs = context?.getSharedPreferences(
+        "previousNewPostContent", Context.MODE_PRIVATE
+    )
 
 
     override fun onCreateView(
@@ -33,29 +39,53 @@ class PostContentFragment : Fragment() {
         binding.postTextAndUrlActivity.edit.requestFocus()
         binding.postTextAndUrlActivity.edit.setText(args.initialContent?.content)
         binding.postTextAndUrlActivity.videoUrl.setText(args.initialContent?.videoUrl)
+        // Ниже: если null, то мы сюда пришли не из редактирования, значит читаем сохраненный из преференс
+        if (args.initialContent?.content == null) {
+            val content = prefs?.getString(SAVED_POST_KEY, null)
+            val previousNewPostContent: PostEditableContent? = if (content != null) {
+                Json.decodeFromString(content) } else null
+                binding.postTextAndUrlActivity.edit.setText(previousNewPostContent?.content)
+                binding.postTextAndUrlActivity.videoUrl.setText(previousNewPostContent?.videoUrl)
+        }
+
+
+        //TODO по кнопке назад сохранение в Преференс
+        //TODO не проходит код внутри edit по кнопке "назад"
+        requireActivity().onBackPressedDispatcher.addCallback(this) {
+                prefs?.edit {
+                    val currentText = binding.postTextAndUrlActivity.edit.text.toString()
+                    val curerntUrl = binding.postTextAndUrlActivity.videoUrl.text.toString()
+                    val currentPost =
+                        Json.encodeToString(PostEditableContent(currentText, curerntUrl))
+                    putString(SAVED_POST_KEY, currentPost)
+                }
+            activity?.onBackPressed()
+//            findNavController().popBackStack()
+        }
 
         binding.ok.setOnClickListener {
             if (!binding.postTextAndUrlActivity.edit.text.isNullOrBlank()) {
                 val text = binding.postTextAndUrlActivity.edit.text.toString()
                 val url = binding.postTextAndUrlActivity.videoUrl.text.toString()
                 viewModel.onSaveButtonClick(text, url)
-//                val resultBundle = Bundle(2)
-//                resultBundle.putString(CONTENT_KEY, text)
-//                resultBundle.putString(URL_KEY, url)
-//                setFragmentResult(REQUEST_KEY, resultBundle)
             }
+        //в этот момент очищать prefs, чтобы при создании нового поста не брать опять данные из старого
+            prefs?.edit()?.clear()
             findNavController().popBackStack() // уходим назад с этого фрагмента
 
         }
     }.root
 
     companion object {
+        // для получения из фрагмента контента при редактировании
         const val REQUEST_KEY = "RequestKey"
         const val CONTENT_KEY = "PostContent"
         const val URL_KEY = "PostUrl"
 
-//        private const val INITIAL_TEXT_KEY = "PostInitialText"
-//        private const val INITIAL_URL_KEY = "PostInitialUrl"
+        // Для преференс при нажатии назад
+        private const val SAVED_TEXT_KEY = "PostInitialText"
+        private const val SAVED_URL_KEY = "PostInitialUrl"
+        private const val SAVED_POST_KEY = "PostInitialPost"
 
 
         // функция, которую мы вызываем для создания Фрагмента. Потом из аргументов достаем данные (см onCreateView)
