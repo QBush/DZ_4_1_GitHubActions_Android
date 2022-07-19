0a4e239c9c41c05bf2cf4e3147f63cc27eff1c15
QBush <ignor-KuBush@yandex.ru>
2022-07-19 11:14:42 +1000

    DZ12 SQL ДЗ Сделано

--- app/src/main/java/ru/netology/nmedia/PostEditableContent.kt
+++ app/src/main/java/ru/netology/nmedia/PostEditableContent.kt
@@ -2,8 +2,10 @@ package ru.netology.nmedia
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
+import kotlinx.serialization.Serializable
 import java.net.HttpURLConnection
 
+@Serializable
 @Parcelize
 class PostEditableContent(
     val content: String?,
--- app/src/main/java/ru/netology/nmedia/UI/PostContentFragment.kt
+++ app/src/main/java/ru/netology/nmedia/UI/PostContentFragment.kt
@@ -25,9 +25,6 @@ class PostContentFragment : Fragment() {
     private val viewModel by viewModels<PostViewModel>(ownerProducer = ::requireParentFragment)
     private val args by navArgs<PostContentFragmentArgs>()
 
-    private val prefs = context?.getSharedPreferences(
-        "previousNewPostContent", Context.MODE_PRIVATE
-    )
 
 
     override fun onCreateView(
@@ -36,31 +33,34 @@ class PostContentFragment : Fragment() {
         savedInstanceState: Bundle?
     ) = PostContentFragmentBinding.inflate(layoutInflater, container, false).also { binding ->
 
+        val previuosContent = context?.getSharedPreferences(
+            "previousNewPostContent", Context.MODE_PRIVATE
+        )
+
         binding.postTextAndUrlActivity.edit.requestFocus()
         binding.postTextAndUrlActivity.edit.setText(args.initialContent?.content)
         binding.postTextAndUrlActivity.videoUrl.setText(args.initialContent?.videoUrl)
         // Ниже: если null, то мы сюда пришли не из редактирования, значит читаем сохраненный из преференс
         if (args.initialContent?.content == null) {
-            val content = prefs?.getString(SAVED_POST_KEY, null)
+            val content = previuosContent?.getString(SAVED_POST_KEY, null)
             val previousNewPostContent: PostEditableContent? = if (content != null) {
-                Json.decodeFromString(content) } else null
-                binding.postTextAndUrlActivity.edit.setText(previousNewPostContent?.content)
-                binding.postTextAndUrlActivity.videoUrl.setText(previousNewPostContent?.videoUrl)
+                Json.decodeFromString(content)
+            } else null
+            binding.postTextAndUrlActivity.edit.setText(previousNewPostContent?.content)
+            binding.postTextAndUrlActivity.videoUrl.setText(previousNewPostContent?.videoUrl)
         }
 
-
-        //TODO по кнопке назад сохранение в Преференс
         //TODO не проходит код внутри edit по кнопке "назад"
         requireActivity().onBackPressedDispatcher.addCallback(this) {
-                prefs?.edit {
-                    val currentText = binding.postTextAndUrlActivity.edit.text.toString()
-                    val curerntUrl = binding.postTextAndUrlActivity.videoUrl.text.toString()
+            val currentText = binding.postTextAndUrlActivity.edit.text.toString()
+            val curerntUrl = binding.postTextAndUrlActivity.videoUrl.text.toString()
+            previuosContent?.edit {
                     val currentPost =
                         Json.encodeToString(PostEditableContent(currentText, curerntUrl))
                     putString(SAVED_POST_KEY, currentPost)
-                }
-            activity?.onBackPressed()
-//            findNavController().popBackStack()
+            }
+//            requireActivity().onBackPressed()
+            findNavController().popBackStack()
         }
 
         binding.ok.setOnClickListener {
@@ -69,8 +69,8 @@ class PostContentFragment : Fragment() {
                 val url = binding.postTextAndUrlActivity.videoUrl.text.toString()
                 viewModel.onSaveButtonClick(text, url)
             }
-        //в этот момент очищать prefs, чтобы при создании нового поста не брать опять данные из старого
-            prefs?.edit()?.clear()
+            //в этот момент очищать prefs, чтобы при создании нового поста не брать опять данные из старого
+            previuosContent?.edit()?.clear()?.commit()
             findNavController().popBackStack() // уходим назад с этого фрагмента
 
         }
